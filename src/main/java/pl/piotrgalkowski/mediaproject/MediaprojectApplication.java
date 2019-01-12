27445166507aac6e6c0e7ac6f00cf62595f0bd74
Piotr Gałkowski <piotrgalkowski81@gmail.com>
2019-01-13 00:06:41 +0100

    initial commit

---
+++ src/main/java/pl/piotrgalkowski/mediaproject/MediaprojectApplication.java
@@ -0,0 +1,14 @@
+package pl.piotrgalkowski.mediaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaprojectApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pl/piotrgalkowski/mediaproject/MediaprojectApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.piotrgalkowski.mediaproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MediaprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
